79bd012892a4e373ca51497a2f10c92b9af95fb7
traian-007 <72014521+traian-007@users.noreply.github.com>
2021-05-13 22:36:57 +0300

    homework2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import cn from 'classnames';
-import s from './App.modules.scss';
-import './custom.css';
+import s from './App.module.scss';
 
 const App = () => {
     return (
